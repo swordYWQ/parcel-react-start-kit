47e88c9344cd318f386877487b02b4cb2db699bf
swordYWQ <86549505@qq.com>
2018-03-30 09:44:53 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+import './style.scss';
+
+class App extends Component {
+  render() {
+    return (<h1 class="text">hello,world</h1>)
+  }
+}
+
+export default App
